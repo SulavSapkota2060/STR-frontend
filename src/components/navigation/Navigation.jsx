bec18b090b461433e5899ff578e6313ca028dabb
Hems Bhandari <hemsbhandari04@gmail.com>
2022-06-19 16:59:43 +0545

    Make nav bar responsive
    and add placeholder for club logo

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,18 +4,20 @@ import './Navigation.css'
 
 const Navigation = () => {
 
-    return(
-        <nav>
-  <div class="nav-container">
-    <ul>
-      <li><a href="#">Work</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Careers</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </div>
-</nav>
-    );
+  return (
+    <nav>
+      <h1>
+        STR Club
+      </h1>
+      <ul>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Careers</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+    </nav>
+  );
 }
 
 export default Navigation;
