54715f6997a89076cd4594c0385a0401a87a0e08
Sulav Sapkota <sulavsapkota001@gmail.com>
2022-06-20 20:23:19 +0545

    Minor Changes

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fir">
+        <div className="navigate"></div>
+        <div className="map"></div>
+        <div className="about"></div>
+      </div>
+      <div className="sec">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
