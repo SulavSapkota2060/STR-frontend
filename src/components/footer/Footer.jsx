4498e30f700dff1d1c7907705f30391fe9ed9598
Hems Bhandari <hemsbhandari04@gmail.com>
2022-06-20 22:28:02 +0545

    Add footer section

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div><p>Footer</p></div>
+  )
+}
+export default Footer;
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -5,6 +5,7 @@ import About from '../../components/about/About';
 import Projects from '../../components/projects/Projects';
 import Team from '../../components/team/Team'
 import Contact from '../../components/contact/Contact'
+import Footer from '../../components/footer/Footer'
 
 
 const Layout = () => {
@@ -18,6 +19,7 @@ const Layout = () => {
            <Projects />
            <Team />
            <Contact />
+           <Footer />
        </div>
     );
 }
