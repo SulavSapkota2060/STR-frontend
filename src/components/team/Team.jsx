9b65a25d62a43a5e6d12c510fcc632a8a64ccc3b
Hems Bhandari <hemsbhandari04@gmail.com>
2022-06-21 18:37:54 +0545

    Format Team.jsx

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -6,65 +6,65 @@ import ProfileContainer from '../ProfileContainer/ProfileContainer'
 const Team = () => {
     const members = [
         {
-            "name":"Sulav",
-            "position":"President"
+            "name": "Sulav",
+            "position": "President"
         }, {
-            "name":"Anish",
-            "position":"Vice President"
+            "name": "Anish",
+            "position": "Vice President"
         }, {
-            "name":"Smritee",
-            "position":"Vice President"
+            "name": "Smritee",
+            "position": "Vice President"
         }, {
-            "name":"Hemanta",
-            "position":"Member"
+            "name": "Hemanta",
+            "position": "Member"
         }, {
-            "name":"Raymod",
-            "position":"Member"
+            "name": "Raymod",
+            "position": "Member"
         },
     ]
-  return (
-    <div className='container'>
-        <h1>
-            Our Team
-        </h1>
+    return (
+        <div className='container'>
+            <h1>
+                Our Team
+            </h1>
 
-        <div className="coordinators">
-            <div className="coordinator">
-                <img src="https://pbs.twimg.com/media/E2K8BVYVUAExUiE?format=jpg&name=360x360" alt="" />
-                <h3>Mr. Hari Ram Tiwari </h3>
-            </div>
-            <div className="coordinator">
-            <img src="https://pbs.twimg.com/media/E2K8BVYVUAExUiE?format=jpg&name=360x360" alt="" />
-                <h3>Mr. Toya Ghimire</h3>
+            <div className="coordinators">
+                <div className="coordinator">
+                    <img src="https://pbs.twimg.com/media/E2K8BVYVUAExUiE?format=jpg&name=360x360" alt="" />
+                    <h3>Mr. Hari Ram Tiwari </h3>
                 </div>
+                <div className="coordinator">
+                    <img src="https://pbs.twimg.com/media/E2K8BVYVUAExUiE?format=jpg&name=360x360" alt="" />
+                    <h3>Mr. Toya Ghimire</h3>
+                </div>
+            </div>
+            <div className="ProfileHolder">
+                <div className="president box">
+                    <ProfileContainer name="Sulav" position="President" />
+                </div>
+                <div className="executive box">
+                    <ProfileContainer name="Sulav" position="President" />
+                    <ProfileContainer name="Sulav" position="President" />
+                    <ProfileContainer name="Sulav" position="President" />
+                    <ProfileContainer name="Sulav" position="President" />
+                </div>
+                <div className="members box">
+                    <ProfileContainer name="Sulav" position="President" showImage="false" />
+                    <ProfileContainer name="Sulav" position="President" showImage="false" />
+
+                    <ProfileContainer name="Sulav" position="President" showImage="false" />
+                    <ProfileContainer name="Sulav" position="President" showImage="false" />
+                    <ProfileContainer name="Sulav" position="President" showImage="false" />
+
+                    <ProfileContainer name="Sulav" position="President" showImage="false" />
+                    <ProfileContainer name="Sulav" position="President" showImage="false" />
+
+                </div>
+
+            </div>
+
         </div>
-   <div className="ProfileHolder">
-   <div className="president box">
-    <ProfileContainer name="Sulav" position = "President" />
-   </div>
-   <div className="executive box">
-   <ProfileContainer name="Sulav" position = "President" />
-   <ProfileContainer name="Sulav" position = "President" />
-   <ProfileContainer name="Sulav" position = "President" />
-   <ProfileContainer name="Sulav" position = "President" />
-   </div>
-<div className="members box">
-<ProfileContainer name="Sulav" position = "President" showImage="false" />
-<ProfileContainer name="Sulav" position = "President" showImage="false" />
-
-<ProfileContainer name="Sulav" position = "President" showImage="false" />
-<ProfileContainer name="Sulav" position = "President" showImage="false" />
-<ProfileContainer name="Sulav" position = "President" showImage="false" />
-
-<ProfileContainer name="Sulav" position = "President" showImage="false" />
-<ProfileContainer name="Sulav" position = "President" showImage="false" />
-
-    </div>       
-
-   </div>
-
-    </div>
-  )
+    )
 }
 
 export default Team;
